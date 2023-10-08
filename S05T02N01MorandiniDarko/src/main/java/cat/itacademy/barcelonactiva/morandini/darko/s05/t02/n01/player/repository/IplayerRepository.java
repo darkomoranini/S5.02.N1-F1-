86ab22c8d1ceee2b3220fa20bc3db8105212da6a
Darko <132366394+darkomoranini@users.noreply.github.com>
2023-10-08 09:52:31 +0200

    Delete S05T02N01MorandiniDarko directory

--- S05T02N01MorandiniDarko/src/main/java/cat/itacademy/barcelonactiva/morandini/darko/s05/t02/n01/player/repository/IplayerRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.morandini.darko.s05.t02.n01.player.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import cat.itacademy.barcelonactiva.morandini.darko.s05.t02.n01.model.domain.player.Player;
-
-public interface IplayerRepository extends JpaRepository<Player, Integer> {
-    Player findByName(String name);
-
-}
